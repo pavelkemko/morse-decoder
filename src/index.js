59a273f6a1bc8f341ebf2a2074591d8489f35f7c
pavelkemko <pavelkemko@gmail.com>
2023-02-07 17:01:33 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   let symbolsArr = [];
+   let result = '';
+   for (let i = 0; i < expr.length; i += 10) {
+    symbolsArr.push(expr.slice(i, i + 10));
+  }
+  for (let j = 0; j < symbolsArr.length; j++) {
+    for (let k = 0; k < symbolsArr[j].length; k++) {
+        while (symbolsArr[j].startsWith('0')) {
+            symbolsArr[j] = symbolsArr[j].replace('0', '');
+        }
+        if (symbolsArr[j][k] === '1' && symbolsArr[j][k + 1] === '0') {
+            symbolsArr[j] = symbolsArr[j].replace('10', '.');
+        } else if (symbolsArr[j][k] === '1' && symbolsArr[j][k + 1] === '1') {
+            symbolsArr[j] = symbolsArr[j].replace('11', '-');
+        } 
+    }
+        if (symbolsArr[j] === '**********') {
+            result += ' '
+        } else {
+            result += MORSE_TABLE[symbolsArr[j]]
+        }
+  }
+  return result;
 }
 
 module.exports = {
